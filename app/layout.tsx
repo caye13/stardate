d89c297d01a1e00c2fe65bcb1cb61d2ea6bea6ff
Cappillen Lee <cappillen@ucsb.edu>
2025-10-16 21:49:50 -0700

    feat: backdrop

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { TRPCReactProvider } from "@/lib/trpc/client";
+import { BackgroundImageManager } from "@/components/BackgroundImageManager";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <BackgroundImageManager />
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
     </html>
